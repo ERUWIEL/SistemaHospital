fb5794f78c70930301bfa6d7e36c967b48ab9ecf
ERUWIEL <angelerubielflores@gmail.com>
2025-04-07 23:41:04 -0600

    interfaz de usuario

---
+++ src/main/java/GUI/utilidades/PButton.java
@@ -0,0 +1,81 @@
+package GUI.utilidades;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Cursor;
+import java.awt.Font;
+import java.awt.Image;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+/**
+ * clase de utilidad que ahorrara codigo Panel Button, es un panel que actua como boton PButton!
+ */
+public class PButton extends JPanel {
+    private Color colorBoton = new Color(4, 14, 79);
+
+    /**
+     * recibe la ruta del icono y opcionalmente del titulo del icono para mostrarse
+     * @param rutaIcono
+     * @param etiqueta
+     */
+    public PButton(String rutaIcono, String etiqueta) {
+        // configuraciones de diseño
+        super(new BorderLayout());
+        setBackground(colorBoton);
+        JLabel texto = new JLabel(etiqueta, JLabel.CENTER);
+        texto.setFont(new Font("Calibri", Font.BOLD, 18));
+        texto.setForeground(new Color(255, 255, 255));
+        texto.setVisible(false);
+        add(texto, BorderLayout.CENTER);
+
+        // configuracion del icono
+        ImageIcon icono = new ImageIcon(rutaIcono);
+        Image iconoFormateado = icono.getImage().getScaledInstance(50, 50, Image.SCALE_SMOOTH);
+        JLabel lblImagen = new JLabel(new ImageIcon(iconoFormateado));
+        add(lblImagen,BorderLayout.NORTH);
+
+        // metodo hover del boton
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                setCursor(new Cursor(Cursor.HAND_CURSOR));
+                setBackground(new Color(23, 2, 99));
+                texto.setVisible(true);
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                setBackground(colorBoton);
+                texto.setVisible(false);
+            }
+        });
+    }
+
+    public PButton(String etiqueta){
+        super();
+        setBackground(colorBoton);
+        JLabel texto = new JLabel(etiqueta, JLabel.CENTER);
+        texto.setFont(new Font("Calibri", Font.BOLD, 18));
+        texto.setForeground(new Color(255, 255, 255));
+        add(texto, BorderLayout.CENTER);
+
+        // metodo hover del boton
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                setCursor(new Cursor(Cursor.HAND_CURSOR));
+                setBackground(new Color(23, 2, 99));
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                setBackground(colorBoton);
+            }
+        });
+    }
+}
---
+++ src/main/java/GUI/utilidades/PMenuMedicos.java
@@ -0,0 +1,49 @@
+package GUI.utilidades;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.GridLayout;
+
+import javax.swing.BorderFactory;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class PMenuMedicos extends JPanel {
+
+    public PMenuMedicos() {
+        super(new BorderLayout());
+        setBackground(new Color(3, 2, 54));
+
+        // panel norte
+        JPanel pnlEncabezado = new JPanel(new GridLayout(2, 1));
+        pnlEncabezado.setOpaque(false);
+        pnlEncabezado.setBorder(BorderFactory.createMatteBorder(0, 0, 1, 0, Color.WHITE));
+
+        // titulo de encabezado
+        JLabel titulo = new JLabel("OPCIONES SOBRE MEDICOS", JLabel.CENTER);
+        titulo.setForeground(Color.WHITE);
+        titulo.setFont(new Font("Arial", Font.BOLD, 18));
+        // botones de opciones
+        JPanel pnlOpciones = new JPanel(new FlowLayout(FlowLayout.LEFT, 15, 0));
+        pnlOpciones.setOpaque(false);
+        PButton btnAgregar = new PButton("Agregar Medicos");
+        PButton btnBuscar = new PButton("Buscar Medicos");
+        PButton btnListar = new PButton("Listar Medicos");
+        pnlOpciones.add(btnAgregar);
+        pnlOpciones.add(btnBuscar);
+        pnlOpciones.add(btnListar);
+        
+        // agregaciones
+        pnlEncabezado.add(titulo);
+        pnlEncabezado.add(pnlOpciones);
+        add(pnlEncabezado, BorderLayout.NORTH);
+
+  
+        // JPanel pnlContenido = new JPanel();
+        JPanel conte = new JPanel();
+        conte.setBackground(new Color(0, 0, 0));
+        add(conte, BorderLayout.CENTER);
+    }
+}
