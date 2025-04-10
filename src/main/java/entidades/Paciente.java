c11097b216c10eda552a2894a49e4406845d7a3c
Luishonshon <luis.angel.castro.lara52@gmail.com>
2025-04-10 15:35:38 -0700

    Actualización de metodo agregar/consultar Paciente, agregando también una nueva clase que xtiende de ObjectOutputStream

--- src/main/java/entidades/Paciente.java
+++ src/main/java/entidades/Paciente.java
@@ -1,11 +1,13 @@
 
 package entidades;
 
+import java.io.Serializable;
+
 /**
  * clase que modela un paciente
  * @author angel erubiel flores jimenez
  */
-public class Paciente {
+public class Paciente implements Serializable{
     private int id;
     private String nombre;
     private int edad;
